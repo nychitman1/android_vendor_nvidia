13ec75513a5c646a98f579b5dbdfb36fc6739aea
Josh Chasky <jchasky@gmail.com>
2018-01-31 11:52:50 -0500

    Dragon: Update blobs to Android 8.1.0 (OPM1.171019.015)
    Change-Id: I1d88f275b118280bb789253ec21b15dd4d244228

--- dragon/device-vendor.mk
+++ dragon/device-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright 2015-2017 The Android Open Source Project
+# Copyright 2015-2018 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DragonKeyboardFirmwareUpdater \
     GCS \
-    HotwordEnrollmentRT5677
+    HotwordEnrollmentOKGoogleRT5677
 
 # Nvidia blob(s) necessary for Dragon hardware
 PRODUCT_COPY_FILES := \
