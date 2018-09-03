e56559f6ed8cf70379c279e4ac9da33bac366c72
followmsi <followmsi@gmail.com>
2018-09-03 07:36:15 -0400

    Add Dragon's Nvidia firmware to vendor.img
    Change-Id: I8c74261f21bbee785f3c97c15377023b257c7dd4

--- dragon/device-vendor.mk
+++ dragon/device-vendor.mk
@@ -28,4 +28,36 @@ PRODUCT_COPY_FILES := \
     vendor/nvidia/dragon/proprietary/etc/permissions/privapp-permissions-platform.xml:system/etc/permissions/privapp-permissions-platform.xml \
     vendor/nvidia/dragon/proprietary/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
     vendor/nvidia/dragon/proprietary/lib/libkeymaster_staging.so:system/lib/libkeymaster_staging.so \
-    vendor/nvidia/dragon/proprietary/lib64/libkeymaster_staging.so:system/lib64/libkeymaster_staging.so
+    vendor/nvidia/dragon/proprietary/lib64/libkeymaster_staging.so:system/lib64/libkeymaster_staging.so \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/cros-pd/dingdong_v1.7.317-b0bb7c9.bin:root/vendor/firmware/cros-pd/dingdong_v1.7.317-b0bb7c9.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/cros-pd/dingdong_v1.7.575-96b74f1.bin:root/vendor/firmware/cros-pd/dingdong_v1.7.575-96b74f1.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/cros-pd/dingdong_v1.7.684-69498dd.bin:root/vendor/firmware/cros-pd/dingdong_v1.7.684-69498dd.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/cros-pd/hoho_v1.7.317-b0bb7c9.bin:root/vendor/firmware/cros-pd/hoho_v1.7.317-b0bb7c9.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/cros-pd/hoho_v1.7.575-96b74f1.bin:root/vendor/firmware/cros-pd/hoho_v1.7.575-96b74f1.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/cros-pd/hoho_v1.7.684-69498dd.bin:root/vendor/firmware/cros-pd/hoho_v1.7.684-69498dd.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/cros-pd/zinger_v1.7.509-e5bffd3.bin:root/vendor/firmware/cros-pd/zinger_v1.7.509-e5bffd3.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/cros-pd/zinger_v1.7.539-91a0fa2.bin:root/vendor/firmware/cros-pd/zinger_v1.7.539-91a0fa2.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/acr_ucode.bin:root/vendor/firmware/nouveau/acr_ucode.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/fecs.bin:root/vendor/firmware/nouveau/fecs.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/fecs_sig.bin:root/vendor/firmware/nouveau/fecs_sig.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/gpmu_ucode_desc.bin:root/vendor/firmware/nouveau/gpmu_ucode_desc.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/gpmu_ucode_image.bin:root/vendor/firmware/nouveau/gpmu_ucode_image.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/nv12b_bundle:root/vendor/firmware/nouveau/nv12b_bundle \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/nv12b_fuc41ac:root/vendor/firmware/nouveau/nv12b_fuc41ac \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/nv12b_fuc41ad:root/vendor/firmware/nouveau/nv12b_fuc41ad \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/nv12b_fuc409c:root/vendor/firmware/nouveau/nv12b_fuc409c \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/nv12b_fuc409d:root/vendor/firmware/nouveau/nv12b_fuc409d \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/nv12b_method:root/vendor/firmware/nouveau/nv12b_method \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/nv12b_sw_ctx:root/vendor/firmware/nouveau/nv12b_sw_ctx \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/nv12b_sw_nonctx:root/vendor/firmware/nouveau/nv12b_sw_nonctx \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/pmu_bl.bin:root/vendor/firmware/nouveau/pmu_bl.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nouveau/pmu_sig.bin:root/vendor/firmware/nouveau/pmu_sig.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nvidia/tegra210/bpmp.bin:root/vendor/firmware/nvidia/tegra210/bpmp.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nvidia/tegra210/nvdec_bl_prod.bin:root/vendor/firmware/nvidia/tegra210/nvdec_bl_prod.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nvidia/tegra210/nvdec_ns.bin:root/vendor/firmware/nvidia/tegra210/nvdec_ns.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nvidia/tegra210/nvdec_prod.bin:root/vendor/firmware/nvidia/tegra210/nvdec_prod.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nvidia/tegra210/nvenc.bin:root/vendor/firmware/nvidia/tegra210/nvenc.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nvidia/tegra210/nvjpg.bin:root/vendor/firmware/nvidia/tegra210/nvjpg.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nvidia/tegra210/tegra_lp0_resume.fw:root/vendor/firmware/nvidia/tegra210/tegra_lp0_resume.fw \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nvidia/tegra210/vic.bin:root/vendor/firmware/nvidia/tegra210/vic.bin \
+    vendor/nvidia/dragon/proprietary/vendor/firmware/nvidia/tegra210/xusb.bin:root/vendor/firmware/nvidia/tegra210/xusb.bin
